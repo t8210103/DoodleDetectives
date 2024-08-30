017200c744d4d662345d91082d51812db03d3db4
t8210103 <t8210103@aueb.gr>
2024-08-30 19:07:06 +0300

    canvas dependencies and basic implementation

---
+++ client/src/components/Canvas.js
@@ -0,0 +1,146 @@
+import React, { useRef, useState } from 'react';
+import { ReactSketchCanvas } from 'react-sketch-canvas';
+import { Eraser, Pen, Redo, Undo, RotateCcw, Save } from 'lucide-react'
+
+export default function Canvas() {
+  const colorInputRef = useRef(null); // No need for HTMLInputElement typing
+  const canvasRef = useRef(null); // No need for ReactSketchCanvasRef typing
+  const [strokeColor, setStrokeColor] = useState('#203354');
+  const [eraseMode, setEraseMode] = useState(false);
+
+  const handleStrokeColorChange = (event) => {
+    setStrokeColor(event.target.value); // Update the state with the new color
+  };
+
+  const handleEraserClick = () => {
+    setEraseMode(true);
+    canvasRef.current?.eraseMode(true);
+  }
+
+  const handlePenClick = () => {
+    setEraseMode(false);
+    canvasRef.current?.eraseMode(false);
+  }
+
+  const handleUndoClick = () => {
+    canvasRef.current?.undo();
+  }
+
+  const handleRedoClick = () => {
+    canvasRef.current?.redo();
+  }
+
+  const handleClearClick = () => {
+    canvasRef.current?.clearCanvas();
+  }
+
+  async function handleSave() {
+    const dataURL = await canvasRef.current?.exportImage('png')
+    if (dataURL) {
+
+      const link = Object.assign(document.createElement('a'), {
+        href: dataURL,
+        style: { display: 'none' },
+        download: 'sketch.png'
+      })
+
+      document.body.appendChild(link)
+      link.click();
+      link.remove();
+    }
+  }
+
+  return (
+    <div className='mt-6 flex max-w-2xl gap-4'>
+      <ReactSketchCanvas
+        width='100%'
+        height='430px'
+        ref={canvasRef}
+        strokeColor={strokeColor}
+        canvasColor='transparent'
+        className='!rounded-2xl !border-purple-500 dark:!border-purple-800'
+      />
+
+      <div className='flex flex-col items-center gap-y-6 divide-y'>
+        {/* color picker */}
+        <button
+          size='icon'
+          onClick={() => colorInputRef.current && colorInputRef.current.click()}
+          style={{ backgroundColor: strokeColor }} // Button background color matches selected color
+        >
+          <input
+            type='color'
+            ref={colorInputRef}
+            style={{
+              backgroundColor: strokeColor,
+              border: 'none',
+              padding: '0px',
+            }}
+            value={strokeColor}
+            onChange={handleStrokeColorChange} // Update color on change
+          />
+        </button>
+
+        {/* drawing mode */}
+        <div className='flex felx-col gap-3 pt-6'>
+          <button
+            size='icon'
+            type='button'
+            variant='outline'
+            disabled={!eraseMode}
+            onClick={handlePenClick}
+          >
+            <Pen size={16}/>
+          </button>
+          <button
+            size='icon'
+            type='button'
+            variant='outline'
+            disabled={eraseMode}
+            onClick={handleEraserClick}
+          >
+            <Eraser size={16}/>
+          </button>
+        </div>
+
+        {/* actions */}
+        <div className='flex flex-col gap-3 pt-6'>
+          <button
+            size='icon'
+            type='button'
+            variant='outline'
+            onClick={handleUndoClick}
+          >
+            <Undo size={16}/>
+          </button>
+          <button
+            size='icon'
+            type='button'
+            variant='outline'
+            onClick={handleRedoClick}
+          >
+            <Redo size={16}/>
+          </button>
+
+          <button
+            size='icon'
+            type='button'
+            variant='outline'
+            onClick={handleClearClick}
+          >
+            <RotateCcw size={16}/>
+          </button>
+
+          <button
+            size='icon'
+            type='button'
+            variant='outline'
+            onClick={handleSave}
+          >
+            <Save size={16}/>
+          </button>
+        </div>
+      </div>  
+    </div>
+  );
+}
--- client/src/components/EachPlayer.js
+++ client/src/components/EachPlayer.js
@@ -1,11 +1,14 @@
+import React from 'react';
+import Canvas from './Canvas';
 
-// implements canva and user details
+// implements canvas and user details
 const EachPlayer = ({ game, userData }) => {
 
     return (
         <div>
             <p> GameId: {game.id} </p>
             <p> ClientId: {userData.clientId} </p>
+            <Canvas/>
         </div>
     );
 }
