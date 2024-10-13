fc182257d46c9924ffd25ff3dc4b9d9e1dd5571b
t8210103 <t8210103@aueb.gr>
2024-10-13 21:44:28 +0300

    Fixed issue with useState

--- client/src/components/WebSocketContext.js
+++ client/src/components/WebSocketContext.js
@@ -7,21 +7,21 @@ const WebSocketContext = createContext();
 export const WebSocketProvider = ({ children }) => {
   const [connected, setConnected] = useState(false);
 
-  // For Server Hosting
-  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket('wss://doodledetectives.pro/ws/', {
-    //queryParams: { username: 'myUsername' },
-    onOpen: () => setConnected(true),
-    onClose: () => setConnected(false),
-    onError: (event) => console.error('WebSocket error:', event)
-  });
-
-  // //For Local Hosting
-  // const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket('ws://localhost:3001', {
+  // // For Server Hosting
+  // const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket('wss://doodledetectives.pro/ws/', {
   //   //queryParams: { username: 'myUsername' },
   //   onOpen: () => setConnected(true),
   //   onClose: () => setConnected(false),
   //   onError: (event) => console.error('WebSocket error:', event)
   // });
+
+  //For Local Hosting
+  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket('ws://localhost:3001', {
+    //queryParams: { username: 'myUsername' },
+    onOpen: () => setConnected(true),
+    onClose: () => setConnected(false),
+    onError: (event) => console.error('WebSocket error:', event)
+  });
 
 
   console.log('WebSocket readyState:', readyState);
