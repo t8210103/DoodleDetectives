ad6d3268eb6f6fc08d3e9e836a8827350d9ea438
t8210103 <t8210103@aueb.gr>
2024-10-13 21:39:59 +0300

    Revert "fixing issue with usestate misconfiguration"
    This reverts commit 2b4df5dc1d786c1cbf9043eb1872ffef0f2092d9.

--- client/src/index.js
+++ client/src/index.js
@@ -9,11 +9,6 @@ Amplify.configure(awsExports);
 
 //import reportWebVitals from './reportWebVitals';
 
-// AWS Amplify with Cognito
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
